53efd5646289353ded475298416a399305163d2b
pala <apalano23@gmail.com>
2025-07-05 00:45:02 +0200

    FEAT: Added base models for events, reservations, and user

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    available_tickets = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from events.models import Event
 
-# Create your models here.
+
+class Reservation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='reservations')
+    reserved_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user} - {self.event}"
